a17de12f372a38141b671ff9ba01e9fdf5bcb73d
Stanley Stuart <stanley@stan.li>
2015-05-04 14:25:57 -0500

    vendor pdftk.tar.gz

--- lib/custom/pdftk.rb
+++ lib/custom/pdftk.rb
@@ -1,3 +1,4 @@
+require 'fileutils'
 class Pdftk < BaseCustom
   def path
     "#{build_path}/vendor/#{name}"
@@ -5,24 +6,24 @@ class Pdftk < BaseCustom
   def name
     "pdftk"
   end
-  def source_url
-    "http://s3.amazonaws.com/source_url_here"
+  def pdftk_path
+    "#{build_path}/vendor/pdftk.tar.gz"
   end
   def used?
     File.exist?("#{build_path}/bin/pdftk") && File.exist?("#{build_path}/bin/lib/libgcj.so.12")
   end
   def compile
     write_stdout "compiling #{name}"
-    #download the source and extract
-    %x{ mkdir -p #{path} && curl --silent #{source_url} -o - | tar -xz -C #{path} -f - } 
+    FileUtils.mkdir_p path
+    Dir.chdir path do
+      %x{ tar -xz -C #{path} -f #{pdfktk_path} }
+    end
     write_stdout "complete compiling #{name}"
   end
   def cleanup!
-    
   end
   def prepare
     File.delete("#{build_path}/bin/lib/libgcj.so.12") if File.exist?("#{build_path}/bin/libgcj.so.12")
     File.delete("#{build_path}/bin/pdftk") if File.exist?("#{build_path}/bin/pdftk")
   end
-  
 end
